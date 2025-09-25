c86ecc2e5a862a49645a9a056e9746b3624e1c89
Nik Soriano <nikaldabasoriano@gmail.com>
2025-09-26 07:39:22 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'sql12.freesqldatabase.com',
-    'port'		=> '3306',
-    'username'	=> 'sql12800136',
-    'password'	=> 'FiKhS25UnU',
-    'database'	=> 'sql12800136',
+    'hostname'	=> getenv("DB_HOST"),
+    'port'		=> getenv("DB_PORT"),
+    'username'	=> getenv("DB_USERNAME"),
+    'password'	=> getenv("DB_PASSWORD"),
+    'database'	=> getenv("DB_NAME"),
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
