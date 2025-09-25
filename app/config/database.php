6a9b9f63f4fc1545c46b48bc70932e531a577972
Nik Soriano <nikaldabasoriano@gmail.com>
2025-09-26 07:22:28 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12798287',
-    'password'	=> '1GyCKUGcIJ',
-    'database'	=> 'sql12798287',
+    'username'	=> 'sql12800136',
+    'password'	=> 'FiKhS25UnU',
+    'database'	=> 'sql12800136',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
